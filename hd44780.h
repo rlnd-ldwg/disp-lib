1f5e3249c9b19b8c31e4ad5f01c7adee23cbe484
rlnd-ldwg <55004103+rlnd-ldwg@users.noreply.github.com>
2019-11-29 10:22:14 +0100

    Update hd44780.h

--- hd44780.h
+++ hd44780.h
@@ -1,5 +1,19 @@
 /*  display library for HD44780 compatible LCD's
-    copyright (c) Roland Ludwig 2012
+    copyright (c) 2012 Roland Ludwig
+    
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
     Version 1.06 (2019-11-18)
 */
