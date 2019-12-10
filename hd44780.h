d9068983c5dda36d8770cf98c9351596540c71f3
rlnd ldwg <zkh.60438@gmail.com>
2019-12-10 20:25:05 +0100

    “v1.1.3/2”

--- hd44780.h
+++ hd44780.h
@@ -15,7 +15,7 @@
     with this program; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-    Version 1.1.2 (2019-12-02)
+    Version 1.1.3 (2019-12-08)
 */
 
 #ifndef HD44780_h
